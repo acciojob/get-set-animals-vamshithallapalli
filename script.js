354af5d10c144cea503ddfa9402b7b58e4c38284
Vamshi Thallapalli <127075912+vamshithallapalli@users.noreply.github.com>
2025-06-14 11:54:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,32 +1,30 @@
 class Animal {
-  private _species: string;
+      constructor(species) {
+        this._species = species;
+      }
 
-  constructor(species: string) {
-    this._species = species; // ✅ Assign to private field
-  }
+      get species() {
+        return this._species;
+      }
 
-  get species(): string {
-    return this._species; // ✅ Return the private field
-  }
+      makeSound() {
+        console.log(`The ${this.species} makes a sound`);
+      }
+    }
 
-  makeSound(): void {
-    console.log(`The ${this.species} makes a sound`);
-  }
-}
+    class Cat extends Animal {
+      purr() {
+        console.log("purr");
+      }
+    }
 
-class Cat extends Animal {
-  purr(): void {
-    console.log("purr");
-  }
-}
+    class Dog extends Animal {
+      bark() {
+        console.log("woof");
+      }
+    }
 
-class Dog extends Animal {
-  bark(): void {
-    console.log("woof");
-  }
-}
-
-// Do not change the code below this line
-window.Animal = Animal;
-window.Cat = Cat;
-window.Dog = Dog;
+    // Do not change the code below this line
+    window.Animal = Animal;
+    window.Cat = Cat;
+    window.Dog = Dog;
